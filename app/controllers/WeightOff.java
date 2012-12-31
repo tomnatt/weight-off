9aedfbdd16fe927d26aef1cdd00ad6d1136dfe61
Tom Natt <github@tomnatt.com>
2012-12-31 18:10:57 +0000

    added current screen

--- app/controllers/WeightOff.java
+++ app/controllers/WeightOff.java
@@ -33,6 +33,7 @@ public class WeightOff extends Controller {
         // get user 
         User user = User.find("byOpenid", session.get("user")).first();
         
+        // TODO some error catching here
         // get weight amount as int
         double wD = Double.parseDouble(weight);
         int wInt = (int)Math.round(wD);
@@ -51,8 +52,24 @@ public class WeightOff extends Controller {
     
     // show results of the current weights
     public static void current() {
-        User user = User.find("byOpenid", session.get("user")).first();
-        render(user);
+        User player1 = User.find("byOpenid", session.get("user")).first();
+        
+        if (player1.inGame()) {
+            /*
+             * TODO note currently we support 2 player games, 
+             * although we don't enforce that
+             */
+
+            // player 1's games, get 1st, get list of opponents, get 1st
+            User player2 = player1.games.get(0).getOpponents(player1).get(0);
+             
+            // if in a game, use the game template (default)
+            render(player1, player2);
+        } else {
+            // else use the single player template
+            render("WeightOff/single.html", player1);
+        }
+        
     }
     
     // show results of the previous week
